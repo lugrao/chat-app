5d0160154765d968fe09d14d11455262cf58bdbd
e <elugrao@gmail.com>
2023-03-14 16:46:29 -0300

    update color palette

--- styles/colors.js
+++ styles/colors.js
@@ -1,7 +1,20 @@
 export const colors = {
-  base: "white",
+  white: "white",
+  grayLight: "#e7e6e9",
+  gray: "#706a79",
   black: "#100720",
+  purpleVeryLight: "#eae6f2",
+  purpleLight: "#836bb0",
   purple: "#31087B",
+  purpleDark: "#1d054a",
+  pinkVeryLight: "#ffeaf8",
+  pinkLight: "#fc82d3",
   pink: "#FA2FB5",
+  pinkDark: "#961c6d",
+  yellowVeryLight: "#fff9ec",
+  yellowLight: "#ffda8a",
   yellow: "#FFC23C",
+  yellowDark: "#997424",
 }
+
+// Useful: https://maketintsandshades.com/
